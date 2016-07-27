4e0db06066f335dfe5ddcb748167ef9bbd5fb5da
Danny Carrillo <odannycx@gmail.com>
2016-07-27 11:17:08 -0700

    Adds basic sse class

---
+++ src/LibSSE.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author Danny Carrillo <odannycx@gmail.com>
+ * @package yii2-sse
+ */
+namespace odannyc\Yii2SSE;
+
+use Sse\SSE;
+use yii\base\Component;
+
+/**
+ * Main wrapper for the LibSSE-php package.
+ */
+class LibSSE extends Component
+{
+    private $sseInstance;
+
+    /**
+     * @param $event
+     * @param $handler
+     */
+    public function addEventListener($event, $handler)
+    {
+        $this->getInstance()->addEventListener($event, $handler);
+    }
+
+    /**
+     * @return null
+     */
+    public function start()
+    {
+        $this->getInstance()->start();
+    }
+
+    /**
+     * @return SSE
+     */
+    private function getInstance()
+    {
+        if ($this->sseInstance == null) {
+            $this->sseInstance = new SSE();
+        }
+
+        return $this->sseInstance;
+    }
+}
