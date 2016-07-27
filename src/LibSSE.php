440671670128d7ced7b3aa69c3a20c0e66eac6a5
Danny Carrillo <odannycx@gmail.com>
2016-07-27 16:28:05 -0700

    Removes all methods and extends on SSE

--- src/LibSSE.php
+++ src/LibSSE.php
@@ -4,44 +4,12 @@
  * @package yii2-sse
  */
 namespace odannyc\Yii2SSE;
-
-use Sse\Event;
 use Sse\SSE;
-use yii\base\Component;
 
 /**
  * Main wrapper for the LibSSE-php package.
  */
-class LibSSE extends Component
+class LibSSE extends SSE
 {
-    private $sseInstance;
-
-    /**
-     * @param $event
-     * @param $handler
-     */
-    public function addEventListener($event, Event $handler)
-    {
-        $this->getInstance()->addEventListener($event, $handler);
-    }
-
-    /**
-     * @return null
-     */
-    public function start()
-    {
-        $this->getInstance()->start();
-    }
-
-    /**
-     * @return SSE
-     */
-    private function getInstance()
-    {
-        if ($this->sseInstance == null) {
-            $this->sseInstance = new SSE();
-        }
-
-        return $this->sseInstance;
-    }
+    //
 }
