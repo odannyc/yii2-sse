713fc6dd2d5a4838f211fe0362689e9a92731641
Danny Carrillo <odannycx@gmail.com>
2016-07-27 12:33:06 -0700

    Adds SSEBase class

---
+++ src/SSEBase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Danny Carrillo <odannycx@gmail.com>
+ * @package yii2-sse
+ * @file SSEBase.php
+ */
+
+namespace odannyc\Yii2SSE;
+
+use Sse\Event;
+
+abstract class SSEBase implements Event
+{
+
+}
